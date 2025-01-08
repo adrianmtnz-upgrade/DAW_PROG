a4fa720055a5fc8bf20e4f76770adf6f09e01f1a
Adrian Martinez <adrian.martinez@bootcamp-upgrade.com>
2025-01-08 20:18:06 +0100

    proyect set-up
    Initialize my git-hub repo

---
+++ BienvenidoAMiRepositorioDAM/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Gracias por tu visita, dentro de poco habran nuevos contenidos!");
+    }
+}
