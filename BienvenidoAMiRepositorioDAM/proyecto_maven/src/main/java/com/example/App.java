cf7fa4cda698344280f0bf30e9519a99c47c969a
Adrian Martinez <adrian.martinez@bootcamp-upgrade.com>
2025-02-12 20:34:22 +0100

    Actualizacion Proyecto Maven

--- BienvenidoAMiRepositorioDAM/proyecto_maven/src/main/java/com/example/App.java
+++ BienvenidoAMiRepositorioDAM/proyecto_maven/src/main/java/com/example/App.java
@@ -1,13 +1,12 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.commons.lang3.StringUtils; // Importamos la clase de Apache Commons Lang
+
+public class App {
+    public static void main(String[] args) {
+        String mensaje = "Hello World!";
+        // Convierte el mensaje a mayúsculas usando StringUtils
+        String mensajeEnMayusculas = StringUtils.upperCase(mensaje);
+        System.out.println(mensajeEnMayusculas);
     }
 }
